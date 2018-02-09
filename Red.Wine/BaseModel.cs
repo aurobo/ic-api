614fb2a9c0049df3298284729b16fcb3991cdd49
Ronak Baldha <ronak.baldha@gmail.com>
2018-02-09 10:25:59 +0530

    Add project Red.Wine

---
+++ Red.Wine/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Red.Wine
+{
+    public class BaseModel
+    {
+        public string LastModifiedBy { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+        public string Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
